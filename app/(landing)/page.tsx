6c73ef92cce7fc455ed84393309464a2272aeb3c
Mahesh kumar Palavalli <p.maheshkumarreddy@gmail.com>
2023-08-16 23:07:43 +0530

    ui fix

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -146,7 +146,7 @@ const Home = () => {
                         <Link legacyBehavior href="https://docs.google.com/spreadsheets/d/1uuVxpWiAthIiWt4l9Djzg1YfTAY_AdxZKiIC5WPugOU/edit">
                             <a className="decoration-blue-500 underline underline-offset-auto" target="_blank" rel="noopener noreferrer">
                                 <p className="p-2 text-blue-600">Link to Excel</p></a></Link></div>
-                    <div className='flex flex-row justify-between'>
+                    <div className='flex flex-row justify-between pb-2'>
                         <div className="p-2 text-2xl font-semibold">Sessions</div>
                         <input type="text" ref={searchButton} onFocus={onFocusHandle} name="search" onChange={onChangeHandle} className="w-1/5 mt-1 px-3 py-2 bg-white border shadow-sm border-grey-300 placeholder-grey-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Search sessions here..." />
                         {/* <DropDownTracks/> */}
